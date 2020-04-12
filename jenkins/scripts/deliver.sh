14d3e50ac92520bff0d1ec52d8af5135fb9c5549
Moon <jangmj@hanafos.com>
2020-04-12 13:18:16 +0900

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,11 +3,13 @@
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-export NAME=`echo "lms"`
+NAME=`echo "lms"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-export VERSION=`echo "0.0.35"`
+VERSION=`echo "0.0.35"`
 set +x
+
+echo 'scp -P 22000 target/${NAME}-${VERSION}.war webdev@1.242.216.122:~/projects/lotte/' > ../../scp.sh
