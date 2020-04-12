797e62830ada9f3959c06723deecb09685fe11b7
MOONJAE <MOONJAE@Jang>
2020-04-12 12:15:33 +0900

    export env var

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,11 +3,11 @@
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`echo "lms"`
+export NAME=`echo "lms"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`echo "0.0.35"`
+export VERSION=`echo "0.0.35"`
 set +x
