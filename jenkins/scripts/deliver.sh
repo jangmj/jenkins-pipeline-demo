b6e562665fb0aa68dc4ec74ce11c3f06ac3af9b9
Moon <jangmj@hanafos.com>
2020-04-12 15:09:09 +0900

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -16,4 +16,6 @@ echo "scp -P 22000 target/${NAME}-${VERSION}.war"
 set -x
 CMD=`scp -P 22000 Jenkinsfile webdev@1.242.216.122:~/projects/lotte/${NAME}-${VERSION}.war`
 set +x
+bash -c 'whoami'
+bash -c 'which scp'
 bash -c '${CMD}'
