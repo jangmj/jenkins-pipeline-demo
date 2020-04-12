ce063b218c2d672e295e2120a1ecdf585cb02886
Moon <jangmj@hanafos.com>
2020-04-12 13:31:53 +0900

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -12,7 +12,7 @@ set -x
 VERSION=`echo "0.0.35"`
 set +x
 
-echo 'scp -P 22000 target/${NAME}-${VERSION}.war'
+echo "scp -P 22000 target/${NAME}-${VERSION}.war"
 set -x
-echo "scp -P 22000 target/${NAME}-${VERSION}.war webdev@1.242.216.122:~/projects/lotte/" > ../../scp.sh
+sh `echo "scp -P 22000 target/${NAME}-${VERSION}.war webdev@1.242.216.122:~/projects/lotte/" > ../../scp.sh`
 set +x
