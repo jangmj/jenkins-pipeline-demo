324619002ad95668b9584f6826353f6779aa3e44
MOONJAE <MOONJAE@Jang>
2020-04-12 12:12:56 +0900

    run shell

---
+++ jenkins/scripts/deliver.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo 'The following complex command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`echo "lms"`
+set +x
+
+echo 'The following complex command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`echo "0.0.35"`
+set +x
