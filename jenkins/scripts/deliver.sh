99cf714b55591ef4782ad127e08e0df859d800cb
MOONJAE <MOONJAE@Jang>
2020-04-16 17:35:03 +0900

    uncommitted files

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,11 +3,11 @@
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-export NAME=`echo "lms"`
+NAME=`echo "lms"`
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-export VERSION=`echo "0.0.35"`
+VERSION=`echo "0.0.35"`
 set +x
